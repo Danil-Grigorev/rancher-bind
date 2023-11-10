ff4098a571de5388a6272073e464d040c696010c
Danil Grigorev <danil.grigorev@suse.com>
2023-11-10 15:06:22 +0000

    apply-globalrole-resource

--- pkg/kubectl/bind-kubeconfig/plugin/kubeconfig.go
+++ pkg/kubectl/bind-kubeconfig/plugin/kubeconfig.go
@@ -6,20 +6,27 @@ import (
 	"crypto/tls"
 	"encoding/base64"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/sethvargo/go-password/password"
 	"golang.org/x/crypto/bcrypt"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/wait"
 
 	apis "github.com/Danil-Grigorev/rancher-bind/pkg/apis"
 	managementv3 "github.com/Danil-Grigorev/rancher-bind/pkg/apis/rancher/management/v3"
 	rbacv1 "k8s.io/api/rbac/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	utilyaml "sigs.k8s.io/cluster-api/util/yaml"
 )
 
 const commonName = "rancher-bind"
@@ -54,21 +61,52 @@ func GenerateRandomPassword() (string, string, error) {
 	return password, hash, err
 }
 
-func createOrUpdate(ctx context.Context, cl client.Client, obj client.Object) error {
+func createOrUpdate(ctx context.Context, cl client.Client, obj client.Object, recreate bool) error {
 	err := cl.Create(ctx, obj)
-	if err != nil {
-		return nil
-	}
-
 	if apierrors.IsAlreadyExists(err) {
-		if err := cl.Update(ctx, obj); err != nil {
-			return fmt.Errorf("unable to update existing object: %w", err)
+
+		if recreate {
+			if err := cl.Delete(ctx, obj); err != nil {
+				return fmt.Errorf("unable to delete existing object: %w", err)
+			}
+
+			if err := wait.PollUntilContextTimeout(ctx, 500*time.Millisecond, 10*time.Second, true, func(ctx context.Context) (done bool, err error) {
+				err = cl.Get(ctx, client.ObjectKeyFromObject(obj), obj)
+				if apierrors.IsNotFound(err) {
+					return true, nil
+				} else if err != nil {
+					return false, err
+				}
+
+				return false, nil
+			}); err != nil {
+				return err
+			}
+
+			err := cl.Create(ctx, obj)
+			if err != nil {
+				return fmt.Errorf("unable to recreate existing object: %w", err)
+			}
+		} else {
+			if err = cl.Get(ctx, client.ObjectKeyFromObject(obj), obj); err != nil {
+				return fmt.Errorf("unable to get object: %w", err)
+			}
+
+			if err := cl.Update(ctx, obj); err != nil {
+				return fmt.Errorf("unable to update existing object: %w", err)
+			}
 		}
 
 		return nil
+	} else if err != nil {
+		return fmt.Errorf("unable to create object: %w", err)
 	}
 
 	return nil
+}
+
+func delete(ctx context.Context, cl client.Client, obj client.Object) error {
+	return cl.Delete(ctx, obj)
 }
 
 func CreateUser(ctx context.Context, cl client.Client, passwordHash string) (*managementv3.User, error) {
@@ -80,7 +118,7 @@ func CreateUser(ctx context.Context, cl client.Client, passwordHash string) (*ma
 		Password: passwordHash,
 	}
 
-	if err := createOrUpdate(ctx, cl, user); err != nil {
+	if err := createOrUpdate(ctx, cl, user, true); err != nil {
 		return nil, fmt.Errorf("unable to create a new user: %w", err)
 	}
 
@@ -130,7 +168,7 @@ func CreateClusterRole(ctx context.Context, cl client.Client, user *managementv3
 		},
 	}
 
-	if err := createOrUpdate(ctx, cl, role); err != nil {
+	if err := createOrUpdate(ctx, cl, role, true); err != nil {
 		return nil, fmt.Errorf("unable to create a new role: %w", err)
 	}
 
@@ -146,7 +184,7 @@ func CreateRoleBinding(ctx context.Context, cl client.Client, user *managementv3
 		UserName:       user.Name,
 	}
 
-	if err := createOrUpdate(ctx, cl, binding); err != nil {
+	if err := createOrUpdate(ctx, cl, binding, true); err != nil {
 		return nil, fmt.Errorf("unable to create a new role binding: %w", err)
 	}
 
@@ -222,4 +260,52 @@ func CollectKubeconfig(serverUrl, token string) (*apis.ConfigResponse, error) {
 	}
 
 	return response, nil
+}
+
+func ApplyUserGlobalRole(ctx context.Context, cl client.Client, username, path string) error {
+	file, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	decoder := utilyaml.NewYAMLDecoder(file)
+	defer decoder.Close()
+
+	u := &unstructured.Unstructured{}
+	_, gvk, err := decoder.Decode(nil, u)
+	if err != nil {
+		return fmt.Errorf("unable to decode provided manifest: %w", err)
+	}
+
+	switch gvk.Kind {
+	case "GlobalRole":
+		role := &managementv3.GlobalRole{}
+		if err := runtime.DefaultUnstructuredConverter.FromUnstructured(u.Object, role); err != nil {
+			return fmt.Errorf("cannot convert object to GlobalRole: %w", err)
+		}
+
+		return applyUserGlobalRole(ctx, cl, username, role)
+	default:
+		return errors.New("unknown resource kind provided")
+	}
+}
+
+func applyUserGlobalRole(ctx context.Context, cl client.Client, username string, role *managementv3.GlobalRole) error {
+	roleBinding := &managementv3.GlobalRoleBinding{ObjectMeta: metav1.ObjectMeta{
+		Name: role.Name,
+	},
+		GlobalRoleName: role.Name,
+		UserName:       username,
+	}
+
+	if err := createOrUpdate(ctx, cl, role, false); err != nil {
+		return fmt.Errorf("unable to create a user role: %w", err)
+	}
+
+	if err := createOrUpdate(ctx, cl, roleBinding, false); err != nil {
+		return fmt.Errorf("unable to create a user role binding: %w", err)
+	}
+
+	return nil
 }
