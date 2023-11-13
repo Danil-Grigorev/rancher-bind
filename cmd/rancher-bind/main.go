7ea3365a6d7b277c9da5a16ea1bbcbc4310742ac
Danil Grigorev <danil.grigorev@suse.com>
2023-11-13 10:52:06 +0100

    scaffold-controller

--- cmd/rancher-bind/main.go
+++ cmd/rancher-bind/main.go
@@ -32,6 +32,8 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	"sigs.k8s.io/controller-runtime/pkg/metrics/server"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
+
+	"github.com/Danil-Grigorev/rancher-bind/internal/controller"
 	//+kubebuilder:scaffold:imports
 )
 
@@ -91,6 +93,13 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&controller.RancherBindReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "RancherBind")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
